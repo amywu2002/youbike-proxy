dcecc458d433601be4409f3f7f3f6f7fb9f0bbac
amywu2002 <amywu2@youbike.com.tw>
2026-01-21 13:10:27 +0800

    Update tycg.js

--- api/tycg.js
+++ api/tycg.js
@@ -1,4 +1,5 @@
 
+// 檔名：api/ping.js
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json({ ok: true, at: new Date().toISOString() });
